7d87f097422b93ee34baa4869a21c80771c8f583
Carlos Muñoz Díaz <47306703G@cchs.csic.es>
2022-05-25 16:56:31 +0200

    Cambio de repo

--- pre/js/charts/charts.js
+++ pre/js/charts/charts.js
@@ -14,7 +14,7 @@ let tooltip = d3.select('#tooltip');
 
 export function initChart() {
     //Lectura de datos
-    d3.csv('https://raw.githubusercontent.com/CarlosMunozDiazCSIC/informe_perfil_mayores_2022_social_4_7/main/data/cuidadores_personas_dependientes_ine_v2.csv', function(error,data) {
+    d3.csv('https://raw.githubusercontent.com/EnvejecimientoEnRed/informe_perfil_mayores_2022_social_4_7/main/data/cuidadores_personas_dependientes_ine_v2.csv', function(error,data) {
         if (error) throw error;
         
         let margin = {top: 12.5, right: 10, bottom: 25, left: 30},
